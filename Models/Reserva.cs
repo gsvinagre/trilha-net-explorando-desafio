7b36b4ecb18b33db31f8614fdc1c11147cd85988
Gustavo Vinagre <gustavovinagre6@gmail.com>
2024-08-19 19:26:26 -0300

    Desafio Hospedagem

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // iMPLEMENTADO
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // IMPLEMENTADO
+                throw new ArgumentException("A capacidade da Suíte é menor que o número de Hóspedes");
             }
         }
 
@@ -35,26 +34,23 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // IMPLEMENTADO
+            return Hospedes != null ? Hospedes.Count : 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+           // IMPLEMENTADO
+            decimal valorTotal = DiasReservados * Suite.ValorDiaria;
+            
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTADO
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valorTotal *= 0.90m;
             }
 
-            return valor;
+            return valorTotal;
         }
     }
 }
